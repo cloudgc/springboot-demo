3508a2a9fddbf32df41dd99da21d3afe1628fbe7
sunzhongwen <sunzhongwen@fullgoalos.com.cn>
2018-12-28 00:15:14 +0800

    add test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,20 +2,23 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+    private final static Logger log = LoggerFactory.getLogger(DemoApplicationTests.class);
 
-	@Test
-	public void contextLoads() {
-
-
-
-
-	}
+    @Test
+    public void contextLoads() {
+        log.info("this is atest ");
+        String test = "test";
+        Assert.notNull(test, "not null");
+    }
 
 }
 
